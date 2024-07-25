fe46fa344f7ef39c304df109f8b2d9fd06f011c6
Yumax-panda <yuma0wall@gmail.com>
2024-07-25 13:33:07 +0900

    eslintを導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,17 @@
-import pluginVue from "eslint-plugin-vue"
+import js from '@eslint/js'
+import eslintPluginVue from 'eslint-plugin-vue'
+import ts from 'typescript-eslint'
 
-export default [
-    ...pluginVue.configs["flat/recommended"]
-]
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...eslintPluginVue.configs['flat/recommended'],
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  }
+)
